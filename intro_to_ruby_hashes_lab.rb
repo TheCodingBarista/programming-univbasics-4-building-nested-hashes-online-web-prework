bbd17b2247bf058adda695c0fdeb0799da319c9a
Melissa Holt <melissa.holt21@gmail.com>
2019-10-27 01:26:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -42,7 +42,9 @@ def monopoly_with_fourth_tier
       },
       :names => {
         :reading_railroad => {},
-        :penn
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        
       }
     },
   }
