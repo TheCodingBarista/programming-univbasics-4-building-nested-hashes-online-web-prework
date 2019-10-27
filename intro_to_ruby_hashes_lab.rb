0e173c9b32171a832df773d777a5d81d6ccfaa61
Melissa Holt <melissa.holt21@gmail.com>
2019-10-27 01:22:52 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,9 @@ def monopoly_with_fourth_tier
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
       },
-      :names => {}
+      :names => {
+        
+      }
     },
   }
 end
