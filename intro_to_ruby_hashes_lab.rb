edb6502bc49f8311c515be32ee23f8d5096cdd62
Melissa Holt <melissa.holt21@gmail.com>
2019-10-27 01:28:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,22 +25,6 @@ def monopoly_with_third_tier
         :four_pieces_owned => 200
       },
       :names => {
-      }
-    },
-  }
-end
-
-def monopoly_with_fourth_tier
-   monopoly = {
-    :railroads => {
-      :pieces => 4,
-      :rent_in_dollars => {
-        :one_piece_owned => 25,
-        :two_pieces_owned => 50,
-        :three_pieces_owned => 100,
-        :four_pieces_owned => 200
-      },
-      :names => {
         :reading_railroad => {},
         :pennsylvania_railroad => {},
         :b_and_o_railroad => {},
@@ -48,4 +32,8 @@ def monopoly_with_fourth_tier
       }
     },
   }
+end
+
+def monopoly_with_fourth_tier
+
 end
